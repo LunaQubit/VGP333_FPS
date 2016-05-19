bac2ce14ee2b25c7ff480db514c6b6d748665fed
WeissGz <rafagda@live.com>
2016-05-18 22:00:56 -0700

    FPS_Game

---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LifeManager : MonoBehaviour
+{
+
+    public int startingLifes;
+    private int lifeCounter;
+    private Text theText;
+
+    void Start()
+    {
+        theText = GetComponent<Text>();
+
+        lifeCounter = startingLifes;     
+    }
+
+
+    void Update()
+    {
+        if(lifeCounter == 0)
+        {
+          //  gameOver();
+        }
+
+        theText.text = "Lifes x" + lifeCounter;
+    }
+
+    public void takeLife()
+    {
+        lifeCounter--;
+    }
+/*
+    public void gameOver()
+    {
+        Application.LoadLevel( 0 );
+    }
+*/
+}
