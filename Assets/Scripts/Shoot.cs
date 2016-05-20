da247a1705b350528e21e253908b74c8184044ef
WeissGz <365162@AIV.EDMC.STU>
2016-05-20 06:42:24 -0700

    Added_Shooting_Script

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour 
+{
+
+    public Rigidbody bullet;
+    public float speed;
+
+	
+	void Start () 
+    {
+	
+	}
+	
+	
+	void Update () 
+    {
+	    if( Input.GetButtonDown( "1" ) )
+        {
+            Rigidbody instantiatedBullet = Instantiate( bullet, transform.position, transform.rotation ) as Rigidbody;
+            instantiatedBullet.velocity = transform.TransformDirection( new Vector3( 0, 0, speed ) );
+        }
+	}
+}
